927b202e0624e723ba456c30437e48ce7b9bb29b
James Katarikawe <james.katarikawe@andela.com>
2020-01-20 10:52:50 +0300

    set up socket.io listener

--- app.js
+++ app.js
@@ -67,5 +67,10 @@ mongoose
   .connect(
     process.env.MONGO_URI)
   .then(result => {
-    app.listen(8080);
-  });
+    const server = app.listen(8080);
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    })
+  })
+  .catch(err => console.log(err));
