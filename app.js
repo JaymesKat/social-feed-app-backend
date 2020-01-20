1e97ca19def5ca6372c6804b74c094d2bcfdf2e5
James Katarikawe <james.katarikawe@andela.com>
2020-01-20 11:54:02 +0300

    refactor socket init code

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ mongoose
     process.env.MONGO_URI)
   .then(result => {
     const server = app.listen(8080);
-    const io = require('socket.io')(server);
+    const io = require('./socket').init(server);
     io.on('connection', socket => {
       console.log('Client connected');
     })
